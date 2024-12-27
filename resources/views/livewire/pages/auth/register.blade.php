65ebe7f4767b786e7dd3cbbe471df4ba4724997a
MoisesCDAW <moisescamposdaw@gmail.com>
2024-12-27 16:30:02 +0000

    Agregadas validaciones extra a la contraseña de usuario al ser registrado: debe contener al menos una letra, un número, una mayús, una minús y algún símbolo.

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -23,7 +23,12 @@ new #[Layout('layouts.guest')] class extends Component
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'string', 'confirmed', Rules\Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+            ],
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
