2fbfe1b6477c8e276bc6a3eb8c876cb5bee5b9f5
MoisesCDAW <moisescamposdaw@gmail.com>
2024-12-28 19:11:00 +0000

    Implementado borrado en cascada para las cables foráneas

--- database/migrations/2024_12_28_184637_create_videogames_table.php
+++ database/migrations/2024_12_28_184637_create_videogames_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('videogames', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/migrations/2024_12_28_184644_create_comments_table.php
+++ database/migrations/2024_12_28_184644_create_comments_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->text('comment');
             $table->integer('score');
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('videogame_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('videogame_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
