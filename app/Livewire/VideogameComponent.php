71040f8e5b22692df5d5dd235d0a3e81306bdebb
MoisesCDAW <moisescamposdaw@gmail.com>
2024-12-29 23:54:08 +0000

    Implementadas funciones para mostrar juegos agregados por el usuario y los de los demás usuarios.

--- app/Livewire/VideogameComponent.php
+++ app/Livewire/VideogameComponent.php
@@ -2,14 +2,47 @@
 
 namespace App\Livewire;
 
+use App\Models\Videogame;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class VideogameComponent extends Component
 {
-    public function render()
-    {
+
+    // The games that the user has
+    public $ownGames = [];
+
+    // The games that the user doesn't have
+    public $otherGames = [];
+
+    /**
+     * Render the component
+     */
+    public function render(){
         return view('livewire.videogame-component');
     }
 
-    
+
+    /**
+     * Get the games that the user has
+     */
+    public function getOwnGames(){
+        return Auth::user()->videogames;
+    }
+
+
+    /**
+     * Get the games that the user doesn't have
+     */
+    public function getOtherGames(){
+        return Videogame::where('user_id', '!=', auth()->user()->id)->get();
+    }
+
+
+    /**
+     * Mount the component
+     */
+    public function mount(){
+        $this->ownGames = $this->getOwnGames();
+    }
 }
--- resources/views/livewire/videogame-component.blade.php
+++ resources/views/livewire/videogame-component.blade.php
@@ -8,48 +8,51 @@
     <div class="p-8">
         <h1 class="text-white text-lg font-bold mb-5" id="own-games">Own games</h1>
         <div class="grid grid-cols-5 2xl:grid-cols-6 gap-8 gap-x-px justify-items-center">  
-            {{-- Card --}}
-            <div>
-                <div>
-                    <!-- Centering wrapper -->
-                    <div class="relative flex flex-col text-white bg-[#3f3fb7] shadow-md bg-clip-border rounded-xl w-60">
 
-                        <div class="px-6 my-4">
-                            <div class="flex items-center justify-between">
-                                <p class="block font-sans antialiased font-medium leading-relaxed text-blue-gray-900">
-                                    Super Mario Bros
-                                </p>
-                                <!-- <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                                    $95.00
+            @foreach ($ownGames as $videogame)
+                {{-- Card --}}
+                <div>
+                    <div>
+                        <!-- Centering wrapper -->
+                        <div class="relative flex flex-col text-white bg-[#222d3d] shadow-md bg-clip-border rounded-xl w-60">
+
+                            <div class="px-6 my-4">
+                                <div class="flex items-center justify-between">
+                                    <p class="block font-sans antialiased font-medium leading-relaxed text-blue-gray-900 whitespace-nowrap overflow-hidden overflow-ellipsis">
+                                        {{$videogame->title}}
+                                    </p>
+                                    <!-- <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                                        $95.00
+                                    </p> -->
+                                </div>
+                                <!-- <p class="block font-sans text-sm antialiased font-normal leading-normal text-white opacity-75">
+                                    With plenty of talk and listen time, voice-activated Siri access, and an
+                                    available wireless charging case.
                                 </p> -->
                             </div>
-                            <!-- <p class="block font-sans text-sm antialiased font-normal leading-normal text-white opacity-75">
-                                With plenty of talk and listen time, voice-activated Siri access, and an
-                                available wireless charging case.
-                            </p> -->
-                        </div>
 
-                        <div class="mx-4 overflow-hidden text-white bg-[#3f3fb7] bg-clip-border rounded-xl h-60 w-52">
-                            <img
-                                src="https://i.3djuegos.com/juegos/16176/new_super_mario_bros_u_deluxe/fotos/ficha/new_super_mario_bros_u_deluxe-4729902.webp"
-                                alt="card-image" class="object-cover w-full h-full" />
-                        </div>
+                            <div class="mx-4 overflow-hidden text-white bg-[#222d3d] bg-clip-border rounded-xl h-60 w-52">
+                                <img
+                                    src="https://i.3djuegos.com/juegos/16176/new_super_mario_bros_u_deluxe/fotos/ficha/new_super_mario_bros_u_deluxe-4729902.webp"
+                                    alt="card-image" class="object-cover w-full h-full" />
+                            </div>
 
-                        <div class="p-3 flex">
-                            <button
-                                class="border-2 border-[#3f3fb7] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
-                                type="button">
-                                Comment
-                            </button>
-                            <button
-                                class="border-2 border-[#3f3fb7] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
-                                type="button">
-                                Details
-                            </button>
+                            <div class="p-3 flex">
+                                <button
+                                    class="border-2 border-[#222d3d] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
+                                    type="button">
+                                    Comment
+                                </button>
+                                <button
+                                    class="border-2 border-[#222d3d] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
+                                    type="button">
+                                    Details
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
             
         </div>
 
@@ -57,49 +60,54 @@
 
         <h1 class="text-white text-lg font-bold mb-5" id="other-games">Other games</h1>
         <div class="grid grid-cols-5 2xl:grid-cols-6 gap-8 gap-x-px justify-items-center">
-            {{-- Card --}}
-            <div>
-                <div>
-                    <!-- Centering wrapper -->
-                    <div class="relative flex flex-col text-white bg-[#3f3fb7] shadow-md bg-clip-border rounded-xl w-60">
 
-                        <div class="px-6 my-4">
-                            <div class="flex items-center justify-between">
-                                <p class="block font-sans antialiased font-medium leading-relaxed text-blue-gray-900">
-                                    Super Mario Bros
-                                </p>
-                                <!-- <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                                    $95.00
+            @foreach ($otherGames as $videogame)
+                {{-- Card --}}
+                <div>
+                    <div>
+                        <!-- Centering wrapper -->
+                        <div class="relative flex flex-col text-white bg-[#222d3d] shadow-md bg-clip-border rounded-xl w-60">
+
+                            <div class="px-6 my-4">
+                                <div class="flex items-center justify-between">
+                                    <p class="block font-sans antialiased font-medium leading-relaxed text-blue-gray-900">
+                                        {{$videogame->title}}
+                                    </p>
+                                    <!-- <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                                        $95.00
+                                    </p> -->
+                                </div>
+                                <!-- <p class="block font-sans text-sm antialiased font-normal leading-normal text-white opacity-75">
+                                    With plenty of talk and listen time, voice-activated Siri access, and an
+                                    available wireless charging case.
                                 </p> -->
                             </div>
-                            <!-- <p class="block font-sans text-sm antialiased font-normal leading-normal text-white opacity-75">
-                                With plenty of talk and listen time, voice-activated Siri access, and an
-                                available wireless charging case.
-                            </p> -->
-                        </div>
 
-                        <div class="mx-4 overflow-hidden text-white bg-[#3f3fb7] bg-clip-border rounded-xl h-60 w-52">
-                            <img
-                                src="https://i.3djuegos.com/juegos/16176/new_super_mario_bros_u_deluxe/fotos/ficha/new_super_mario_bros_u_deluxe-4729902.webp"
-                                alt="card-image" class="object-cover w-full h-full" />
-                        </div>
+                            <div class="mx-4 overflow-hidden text-white bg-[#222d3d] bg-clip-border rounded-xl h-60 w-52">
+                                <img
+                                    src="https://i.3djuegos.com/juegos/16176/new_super_mario_bros_u_deluxe/fotos/ficha/new_super_mario_bros_u_deluxe-4729902.webp"
+                                    alt="card-image" class="object-cover w-full h-full" />
+                            </div>
 
-                        <div class="p-3 flex">
-                            <button
-                                class="border-2 border-[#3f3fb7] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
-                                type="button">
-                                Comment
-                            </button>
-                            <button
-                                class="border-2 border-[#3f3fb7] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
-                                type="button">
-                                Details
-                            </button>
+                            <div class="p-3 flex">
+                                <button
+                                    class="border-2 border-[#222d3d] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
+                                    type="button">
+                                    Comment
+                                </button>
+                                <button
+                                    class="border-2 border-[#222d3d] hover:border-gray-300 font-sans font-bold text-center uppercase disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-2 rounded-lg shadow-gray-900/10 hover:shadow-gray-900/20 active:opacity-[0.85] active:shadow-none block w-full bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none focus:shadow-none"
+                                    type="button">
+                                    Details
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
+
         </div>
+
     </div>
 
 </div>
