58a801251bc09a0ff3acd2c78cc20724aaf4842d
MoisesCDAW <moisescamposdaw@gmail.com>
2024-12-29 13:05:37 +0000

    Se eliminó la relación muchos a muchos entre la tabla Users y Videogames

--- app/Models/Videogame.php
+++ app/Models/Videogame.php
@@ -47,23 +47,4 @@ class Videogame extends Model
     {
         return $this->hasMany(Comment::class, 'foreign_key', 'local_key');
     }
-
-    /**
-     * The roles that belong to the videogame
-     * Many-to-many relationship with User (via Comment)
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function users(): BelongsToMany
-    {
-        return $this->belongsToMany(User::class, 'comments', 'user_id', 'videogame_id')->withTimestamps();
-        
-        /*
-        comments: The name of the pivot table (in this case, comments).
-        user_id: The foreign key in the comments table that references the users table.
-        videogame_id: The foreign key in the comments table that references the videogames table.
-        */
-
-        // withTimestamps: This method is used in many-to-many relationships to allow Laravel to automatically manage the created_at and updated_at columns in the pivot table.
-    }
 }
